a6ec9ccf12e6a7a2d44686af34d34b8e3f569b26
Fabrizio Onorio <onoriofabrizio@gmail.com>
2021-08-24 18:21:39 +0200

    fixed routes

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,21 @@
 class TripsController < ApplicationController
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(flight_params)
+    if @trip.save
+      redirect_to @flight, notice: 'Flight was successfully created'
+    else
+      render :new
+    end
+    @me = current_user
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:date, :departure, :destination, :flight_number)
+  end
 end
