bcc55513885611efaf7b8dad20efd9fd8a8ee75a
Mohammad Alshahwan <shahwanoo@live.com>
2021-08-24 17:53:53 +0200

    partly soft coded the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flights
+  get "/profile", to: 'profile#profile'
+  get "/trips", to: 'trips#index'
 end
