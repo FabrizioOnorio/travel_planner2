319563db22b7ec1c4af29ef98b92f1a117fc34be
Fabrizio Onorio <onoriofabrizio@gmail.com>
2021-08-24 16:56:33 +0200

    create page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,21 @@
 class FlightsController < ApplicationController
+  def new
+    @flight = Flight.new
+  end
+
+  def create
+    @flight = Flight.new(flight_params)
+    if @flight.save
+      redirect_to @flight, notice: 'Flight was successfully created'
+    else
+      render :new
+    end
+    @me = current_user
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:date, :departure, :destination, :flight_number)
+  end
 end
