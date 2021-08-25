5e05075766464a79fcfc525276b70fd35bf0d1a2
Fabrizio Onorio <onoriofabrizio@gmail.com>
2021-08-25 15:47:41 +0200

    worked with flight#create

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,21 +1,29 @@
 class FlightsController < ApplicationController
+
   def new
     @flight = Flight.new
   end
 
   def create
     @flight = Flight.new(flight_params)
+    @return_flight = Flight.new(return_flight_params)
+    @return_flight.date = Date.new(params["flight"]["return_date(1i)"].to_i, params["flight"]["return_date(2i)"].to_i, params["flight"]["return_date(3i)"].to_i)
+    @return_flight.departure = @flight.destination
+    @return_flight.destination = @flight.departure
     if @flight.save
       redirect_to @flight, notice: 'Flight was successfully created'
     else
       render :new
     end
-    @me = current_user
   end
 
   private
 
   def flight_params
     params.require(:flight).permit(:date, :departure, :destination, :flight_number)
+  end
+
+  def return_flight_params
+    params.require(:flight).permit(:return_date, :departure, :destination, :flight_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'profile', to: 'pages#profile'
-  resources :flights
-  resources :trips
+  resources :trips, only: %w[index show new create]
+  resources :flights, only: %w[create new]
 end
