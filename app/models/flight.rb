6144473e0fbdef67af308e05ba7da7b10e90e03c
Benedikt Albertsen <be.albertsen@gmail.com>
2021-08-26 14:28:52 +0200

    no idea what i am pushing

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ApplicationRecord
   attr_accessor :return_date
-  
-  belongs_to :inbound, class_name: "Trip"
-  belongs_to :outbound, class_name: "Trip"
+
+  # belongs_to :inbound, class_name: "Trip"
+  # belongs_to :outbound, class_name: "Trip"
 end
